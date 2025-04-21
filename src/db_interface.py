1fd51ca79d8d5f1bd9379f86144f659929a3a4c2
Barman Roy, Swagato <swagatopablo@aol.com>
2025-04-21 16:48:24 +0800

    Database interface first draft completed

--- src/db_interface.py
+++ src/db_interface.py
@@ -2,24 +2,130 @@
 # encoding: utf-8
 
 """This file presents the abstract interface and implementation of a Database Client."""
+import logging
+import os
 from abc import ABC, abstractmethod
-from typing import Iterable, Optional
+from configparser import ConfigParser
+from contextlib import AbstractContextManager
+from typing import Iterable, Optional, cast, Any, Set
+
 import polars as pl
+from sqlalchemy import create_engine, Engine, Table, MetaData, Delete, delete, Update
+from sqlalchemy.orm import Session, Query
+
+logging.basicConfig(format='%(asctime)s|%(levelname)s: %(message)s',
+                    datefmt='%H:%M:%S, %d-%b-%Y', level=logging.DEBUG)
+
+# Load basic configurations
+config: ConfigParser = ConfigParser()
+assert config.read(filenames='config.ini')
+
+PRIM_KEY: str = config.get(section='columns', option='PRIM_KEY_COL')
+TITLE: str = config.get(section='columns', option='TITLE')
+DETAIL: str = config.get(section='columns', option='DETAIL')
+STATUS: str = config.get(section='columns', option='STATUS')
+DUE: str = config.get(section='columns', option='DUE')
+
+DIALECT: str = config.get(section='db', option='DIALECT', raw=True)
+DRIVER: str = config.get(section='db', option='DRIVER', raw=True)
+DB_USERNAME: str = os.environ['USER']
+DB_PASSWORD: str = os.environ['MYSQL_PASSWORD']
+HOST: str = config.get(section='db', option='HOST', raw=True)
+PORT: int = int(config.get(section='db', option='PORT', raw=True))
+TABLE: str = config.get(section='db', option='TABLE', raw=True)
+DATABASE: str = os.environ['MYSQL_DATABASE']
+HOST_CONNECTOR = (f'{DIALECT}+{DRIVER}://{DB_USERNAME}:{DB_PASSWORD}@{HOST}:'
+                  f'{PORT}')
+URI: str = os.path.join(HOST_CONNECTOR, DATABASE)
+
 
 class AbstractPersistenceInterface(ABC):
     """Define the interface to persist data."""
 
     @abstractmethod
-    async def retrieve_tasks(self, tasks:Optional[Iterable[int]|int]=None)->pl.DataFrame:
-        """Retrieve a collection of tasks, a single task or all tasks."""
+    def retrieve_tasks(self, tasks: Optional[Iterable[int]] = None) -> pl.LazyFrame:
+        """Retrieve a collection of tasks by task id or all tasks."""
         raise NotImplementedError
 
     @abstractmethod
-    async def update_status(self, task:int, new_status:str)->None:
+    def update_status(self, task: int, new_status: str) -> None:
         """Update the status of a task by id."""
         raise NotImplementedError
 
     @abstractmethod
-    async def delete_task(self, task:int)->None:
+    def delete_task(self, task: int) -> None:
         """Delete a task."""
         raise NotImplementedError
+
+
+class EngineContext(AbstractContextManager):
+    """Wrap the connectivity engine inside a context to prevent leakage."""
+
+    def __init__(self, uri: str):
+        """
+        Initialise the class with the connection string.
+        Do not start the engine. Leave it to the context manager to acquire
+        a connection.
+        """
+        self.uri: str = uri
+        self.engine: Optional[Engine] = None
+
+    def __enter__(self) -> Engine:
+        """Create an engine and acquire the connection."""
+        self.engine = cast(typ=Engine, val=create_engine(url=self.uri))
+        logging.debug(msg=f'Engine created from {self.engine.url}.')
+        return cast(typ=Engine, val=self.engine)
+
+    def __exit__(self, exception_type: Any, exception_value: Any,
+                 traceback: Any) -> None:
+        """Release the connection."""
+        self.engine.dispose()
+        logging.debug(msg='Engine disposed.')
+
+
+class DBInterface(AbstractPersistenceInterface):
+    """Implement the asynchronous interface to the MySQL database."""
+
+    def __init__(self, engine: EngineContext):
+        """Initialise the client with an asynchronous engine."""
+        self._engine_: EngineContext = engine
+
+    def retrieve_tasks(self, tasks: Optional[Iterable[int]] = None) -> pl.LazyFrame:
+        """Query the relevant tasks by task ids. If no task id supplied, get all tasks."""
+        with self._engine_ as engine:
+            session: Session = Session(bind=engine)
+            table: Table = Table(TABLE, MetaData(), autoload_with=engine)
+            query: Query = session.query(table.columns[PRIM_KEY], table.columns[TITLE], table.columns[DETAIL],
+                                         table.columns[DUE],
+                                         table.columns[STATUS])
+            if tasks:
+                query = query.filter(table.columns[PRIM_KEY].in_(other=tasks))
+            raw_query: str = str(
+                query.order_by(table.columns[DUE]).statement.compile(compile_kwargs={'literal_binds': True}))
+            logging.debug(msg=f'Retrieving all tasks matching {tasks}.')
+            return pl.read_database(query=raw_query, connection=session).lazy()
+
+    def delete_task(self, task: int) -> None:
+        """Delete a task by task id."""
+        with self._engine_ as engine:
+            table: Table = Table(TABLE, MetaData(), autoload_with=engine)
+            with Session(bind=engine) as session, session.begin():
+                stmt: Delete = delete(table=table).where(table.columns[PRIM_KEY] == task)
+                logging.debug(msg=f'Deleting task id {task}.')
+                session.execute(statement=stmt)
+
+    def update_status(self, task: int, new_status: str) -> None:
+        """Update the status of a task by task id."""
+        allowed: Set[str] = {'PENDING', 'WIP', 'DONE'}
+        try:
+            assert new_status in allowed
+        except AssertionError:
+            message: str = f'Status {new_status} not allowed.'
+            logging.error(msg=message)
+            raise ValueError(message)
+        with self._engine_ as engine:
+            table: Table = Table(TABLE, MetaData(), autoload_with=engine)
+            with Session(bind=engine) as session, session.begin():
+                stmt: Update = table.update().where(table.columns[PRIM_KEY] == task).values(status=new_status)
+                logging.debug(msg=f'Updating task {task} status to {new_status}.')
+                session.execute(statement=stmt)
